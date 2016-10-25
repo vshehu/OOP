187c291e653e40c2f03494f318dac184ba0e7e65
ervin007 <ervinshaqiri@gmail.com>
2016-10-25 15:23:30 +0200

    Update Student.h

--- First Project/First project/Student.h
+++ First Project/First project/Student.h
@@ -1,10 +1,49 @@
 #include <string>
+#include<iostream>
 
 #ifndef STUDENT_H
 #define STUDENT_H
+using namespace std;
+
+struct PlaceOfLiving
+{
+	string City;
+	string Country;
+	string Street;
+};
+
 struct Student
 {
-
+	string ID;
+	string FirstName;
+	string LastName;
+	string Department;
+	string email;
+	PlaceOfLiving Addres;
+	string Birthday;
 };
+
+
+
+void SeerchItUp(Student *a, double n)
+
+{
+	cout<<"Enter a search term..."<<endl;
+	string m;
+	cout<<endl;
+	cin>>m;
+	for(double i=0; i<n;i++)
+	{
+		if(a[i].ID==m)
+		a[i].PrintInformation();
+		
+		else if(a[i].FirstName==m)
+		a[i].PrintInformation();
+		
+		else if(a[i].LastName==m)
+		a[i].PrintInformation();
+
+	}
+}
 
 #endif
