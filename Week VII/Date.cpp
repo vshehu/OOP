b76309fa3583eff627e4b6c3c7ce993c64b5907e
ervin007 <ervinshaqiri@gmail.com>
2016-12-05 21:01:16 +0100

    Update Date.cpp

--- Week VII/Date.cpp
+++ Week VII/Date.cpp
@@ -1,104 +1,248 @@
+
+
+
+
+///author ervin shaqiri 124384
+
+/// rdate.h file a header file and its class
+
 #include <iostream>
 #include <string>
+
 using namespace std;
 
-class Date
+#ifndef   rdate_h
+#define   rdate_h
+
+class date
 {
-private:
-	int _day;
-	int _month;
-	int _year;
-public:
-	void setDay(int day)
-	{
-		_day = day;
-	}
-	int getDay()
-	{
-		return _day;
-	}
-	void setMonth(int month)
-	{
-		_month = month;
-	}
-	int getMonth()
-	{
-		return _month;
-	}
-	void setYear(int year)
-	{
-		_year = year;
-	}
-	int getYear()
-	{
-		return _year;
-	}
-	void Print()
-	{
-		cout << _year << "/" << _month << "/" << _day;
-	}
-
-	void Print(string locale)
-	{
-		if (locale == "US")
-			cout << _month << "/" << _day << "/" << _year;
-		else if (locale == "MK")
-			cout << _day << "/" << _month << "/" << _year;
-		else 
-			cout << _year << "/" << _month << "/" << _day;
-	}
-
-	Date()
-	{
-		_year = 0;
-		_month = 1;
-		_day = 1;
-	}
-	Date(int year, int month, int day)
-	{
-		_year = year;
-		_month = month;
-		_day = day;
-	}
-
-	bool Equals(Date d)
-	{
-		if (_day == d.getDay() && _month == d.getMonth() && _year == d.getYear())
+	private:
+		int _date;
+		int _month;
+		int _year;
+	public:
+		int getdate()
 		{
-			return true;
-		}
-		return false;
-	}
-
-	bool operator==(Date d)
-	{
-		if (_day == d.getDay() && _month == d.getMonth() && _year == d.getYear())
+			return _date;
+		};
+		void setdate(int date)
 		{
-			return true;
-		}
-		return false;
-	}
-
-	Date operator--(int)
-	{
-		if (_day >= 1)
+			_date = date;
+		};
+		int getyear()
 		{
-			_day--;
-			return Date(_year, _month, _day);
-		}
-		else
+			return _year;
+		};
+		void setyear(int year)
 		{
-			throw exception("Date cannot be 0");
+			_year = year;
+		};
+		int getmonth()
+		{
+			return _month;
+		};
+		void setmonth(int month)
+		{
+			_month = month;
+		};
+		void printdate(string locale);
+		date()
+		{
+			_date = 1;
+			_year = 0;
+			_month = 1;
 		}
-		
-	}
+		date :: date(int date, int year, int month)
+		{
+			_date = date;
+			_year = year;
+			_month = month;
+		};
+		bool equals(date d)
+		{
+			if(_date == d.getdate() && _year == d.getyear() && _month == d.getmonth())
+				return true;
+			else 
+				return false;
+		};
+		bool operator==(date d)
+		{
+			if(_date == d.getdate() && _year == d.getyear() && _month == d.getmonth())
+				return true;
+			else 
+			{
+				return false;
+			};
+		date operator++(int date, int year, int month)
+		{
+			if (_day == 31 && _month == 12)
+			{
+				_year++;
+				_day = 1;
+				_month = 1;
+			};
+			if((_month == 1 || _month == 3 || _month == 5 || _month ==  7 || _month == 8 || _month == 10 || _month == 12) && _date == 31)
+			{
+				_month++;
+				_date = 1;
+				return date(int date, int year, int month);
+			};
+			else
+			{
+				_date++;
+				return date(int date,int year, int month);
+			};
+			if((_month == 4 || _month == 6 || _month == 9 || _month == 11) && _day == 30)
+			{
+				_month++;
+				_date = 1;
+				return date(int date, int year, int month);
+			};
+			if(_year%4 ==  0)
+			{
+				if(_day == 28)
+				{
+					_month+;
+					_date = 1;
+					return date(int date, int year, int month);
+				};
+				else
+				{
+					_date++;
+					return date(int date, int year, int month);
+				};
+			};
+			else 
+			{
+				if(_date == 27)
+				{
+					_month++;
+					_date = 1;
+					return date(int date, int year, int month);
+				};
+				else
+				{
+					_date ++;
+					return date(int date, int yaer, int month);
+				};
+			};
+		};
+		date operator--(int date, int year, int month)
+		{
+			if(_month == 1 && _day == 1)
+			{
+				_year--;
+				_day = 31;
+				_month = 12;
+			};
+			if((_month == 2 || _month == 4 || _month == 6 || _month == 9 || _month == 11) && _day == 1)
+			{
+				_day = 31;
+				_month--;
+			};
+			else if(_month == 5 || _month == 7 || _month == 8 || _month == 10 || _month == 12 && _day== 1)
+			{
+				_day = 30;
+				_month--;
+			};
+			else if(_year%4 == 0)
+			{
+				if(_month == 3 && _day==1)
+				{
+					_day = 28;
+					_month--;
+				};
+				else
+				{
+					_day = 27;
+					_month--;
+				};
+			};
+		}
+		date operator +(const date &p);
+		{
+			date date;
+			date._year = this.->_year + p.getyear();
+			date._day = this.->_day + p.getyear();
+			date._month = this.->_month + p.getmonth();
+		};
+       date operator -(const date &p)
+       {
+		date date;
+		date._year = this-> _year - p.getyear();
+		date._day = this-> _day - p.getyear();
+		date._month = this-> _month - p.getmonth();
+	   };
+	    date operator >(const date &p)
+	    {
+				date date;		 	
+				if(this.->_year > p.getyear())
+					return date.setyear()=_year;
+				else
+					return date.setyear()= p._year;
+				if(this.->_day > p.getday())
+					return date.setday() = _day;
+				else
+					return date.setmonth() = p._day;
+				if (this.->date._month > p.getmonth())
+					return date.setmonth() = _month;
+				else
+					return date.setmonth() = p._month;
+	    };
+		date operator <(const date &p)
+		{
+				date date;
+				if(this.->_year < p.getyear())
+					return date.setyear() = p.getyear();
+				else
+					return date.setyear() = _year;
+				if(this.->_day < p.getday())
+					return date.setday() = p.getday();
+				else
+					return date.setday() = _day;
+				if(this.->_month < p.getmoth)
+					return date.setmonth() = p.getmonth;
+				else 
+					return date.setmonth() = _month;
+		};
+	};
 };
+
+#endif
+
+/// cpp file for rdate.h.
+
+#include <iostream>
+#include <string>
+#include "rdate.h"
+
+using namespace std;
+void date :: printdate(string locale)
+{
+	if(locale =="us")
+		cout << getdate() << "/" << getmonth() << "/" << getyear() << endl;
+	else if (locale == "mk")
+		cout << getmonth() << "/" << getyear() << "/" << getdate() << endl;
+	else
+		cout << getmonth() << "/" << getyear() << "/" << getdate() << endl;
+};
+int date :: date operator +
+
+/// main file.
+#include <iostream>
+#include <string>
+#include "rdate.h"
+
+using namespace std;
 
 int main()
 {
-	Date d(2016, 11, 1);
-	d.Print();
-	d--;
-	cout << endl;
-	d.Print();
-}
+	date p;
+	string locale = "us";
+	p.setdate(0);
+	p.setyear(0);
+	p.setmonth(0);
+	p.printdate(locale);
+	system("pause");
+	return 0;
+};
